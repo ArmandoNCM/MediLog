e839692c755621fe6e7ee9fc846c2ba48acc4a98
Armando Castillo <armando.castillo@sundevs.com>
2019-03-25 20:02:55 -0500

    Updated database model by changing client_habits table name from plural to singular form Created HabitPersistence class

---
+++ Implementation/MediLog/src/persistence/entityPersisters/HabitPersistence.java
@@ -0,0 +1,81 @@
+package persistence.entityPersisters;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Habit;
+import persistence.Database;
+
+public class HabitPersistence {
+	
+	private static final String SELECT_CLIENT_HABITS_QUERY = 
+			"SELECT " + 
+			"    habit.id_habit, " + 
+			"    habit.description, " + 
+			"    client_habit.weekly_hours_intensity, " + 
+			"    client_habit.registered_on " + 
+			"FROM habit " + 
+			"JOIN client_habit " + 
+			"    ON habit.id_habit = client_habit.habit " + 
+			"WHERE client_habit.client = ?";
+	
+	private static final String SELECT_HABITS_QUERY = 
+			"SELECT " + 
+			"    id_habit, " + 
+			"    description " + 
+			"FROM habit";
+	
+	public static List<Habit> getClientHabits(String clientId) throws SQLException {
+		// Create prepared statement with parameterized query
+		PreparedStatement preparedStatement = Database.getInstance().getConnection().prepareStatement(SELECT_CLIENT_HABITS_QUERY);
+		// Set query parameters
+		preparedStatement.setString(1, clientId);
+		// Execute query
+		ResultSet resultSet = preparedStatement.executeQuery();
+		
+		List<Habit> habits = new ArrayList<Habit>();
+		// Iterate over the result set
+		while (resultSet.next()) {
+			// Retrieve column data
+			int habitId = resultSet.getInt(1);
+			String description = resultSet.getString(2);
+			int weeklyHoursIntensity = resultSet.getInt(3);
+			LocalDate registeredOn = resultSet.getDate(4).toLocalDate();
+			// Instantiate Habit object
+			Habit habit = new Habit();
+			habit.setId(habitId);
+			habit.setDescription(description);
+			habit.setWeeklyHoursIntensity(weeklyHoursIntensity);
+			habit.setRegisteredOn(registeredOn);
+			// Add to object to list
+			habits.add(habit);
+		}
+		return habits;
+	}
+	
+	public static List<Habit> getHabitTypes() throws SQLException {
+		// Create a simple statement and run query
+		Statement statement = Database.getInstance().getConnection().createStatement();
+		ResultSet resultSet = statement.executeQuery(SELECT_HABITS_QUERY);
+		List<Habit> habits = new ArrayList<Habit>();
+		// Iterate over the result set
+		while(resultSet.next()) {
+			// Retrieve column data
+			int habitId = resultSet.getInt(1);
+			String description = resultSet.getString(2);
+			// Instantiate Habit object
+			Habit habit = new Habit();
+			habit.setId(habitId);
+			habit.setDescription(description);
+			// Add object to list
+			habits.add(habit);
+		}
+		return habits;
+	}
+
+}
