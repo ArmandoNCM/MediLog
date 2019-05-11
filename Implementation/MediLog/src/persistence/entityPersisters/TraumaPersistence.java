053870201b5f9abb05212469e4129ec7e5a1aed3
Santiago Ruiz <chuchonet19@hotmail.com>
2019-05-11 10:47:03 -0500

    Started creation of TraumaPersistence class

---
+++ Implementation/MediLog/src/persistence/entityPersisters/TraumaPersistence.java
@@ -0,0 +1,75 @@
+package persistence.entityPersisters;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Trauma;
+import persistence.Database;
+
+public class TraumaPersistence {
+	
+	private static final String  SELECT_QUERY =
+			"SELECT  " +  
+			"    id_trauma_history, " +
+			"    trauma_nature, " + 
+			"    sequels, " + 
+			"    occurrence_date, " + 
+			"    registered_on " + 
+			"FROM " + 
+			"    trauma_history " + 
+			"WHERE " + 
+			"    client = ?";
+			
+	
+	private static final String INSERT_QUERY = 
+			"INSERT INTO trauma_history " + 
+			"	( " + 
+			"		id_trauma_history, " + 
+			"		client, " + 
+			"		trauma_nature, " + 
+			"		sequels, " + 
+			"		ocurrence_date, " + 
+			"		registered_on " +
+			"	) " + 
+			"VALUES(?,?,?,?,?,?)";
+	
+	public static List<Trauma> loadTraumas(String clientId) throws SQLException {
+		// Create prepared statement with parameterized query
+		PreparedStatement preparedStatement = Database.getInstance().getConnection().prepareStatement(SELECT_QUERY);
+		// Set query parameters
+		preparedStatement.setString(1, clientId);
+		// Execute query
+		ResultSet resultSet = preparedStatement.executeQuery();
+	
+		List<Trauma> traumas=new ArrayList<Trauma>(); 
+		// Iterate over the results
+		while (resultSet.next()) {
+			// Retrieve column data
+			int traumaId = resultSet.getInt(1);
+			String traumaNature = resultSet.getString(2);
+			String sequels = resultSet.getString(3);
+			LocalDate occurrenceDate = resultSet.getDate(4).toLocalDate();
+			LocalDate registeredOn = resultSet.getDate(5).toLocalDate();
+			
+			// Instantiate the Trauma object
+			Trauma trauma = new Trauma();
+			trauma.setId(traumaId);
+			trauma.setTraumaNature(traumaNature);
+			trauma.setSequels(sequels);
+			trauma.setOccurrenceDate(occurrenceDate);
+			trauma.setRegisteredOn(registeredOn);
+			
+			// Add the trauma to the list of traumas
+			traumas.add(trauma);
+		}
+
+		return traumas;
+	}
+			
+	
+
+}
